bafe321e3f9eb0488e08d7169618eb77dbd168a7
Luan Leite Leão <luan.leite@squadra.com.br>
2022-07-17 17:05:29 -0300

    interface user repository

--- src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -1,5 +1,6 @@
 package br.com.dicasdeumdev.api.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,13 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.services.UserService;
+import br.com.dicasdeumdev.api.services.impl.UserServiceImpl;
 
 @RestController
 @RequestMapping("/user")
 public class UserResource {
+
+    @Autowired
+    private UserService service;
     
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok(new User(id,"luan leite leao","luan@luan.com","123"));
+        return ResponseEntity.ok(service.findById(id));
     }
 }
